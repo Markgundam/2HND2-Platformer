ed969748a0e89c2ab2783a5b7a680f164482b747
Mark Andrew Azzopardi <mark25gundam@hotmail.com>
2014-03-24 14:02:38 +0100

    Added player
    Added player

---
+++ Assets/Assets/Scripts/PlayerController.js
@@ -0,0 +1,70 @@
+﻿#pragma strict
+
+private var grounded:boolean;
+private var anim:Animator;
+
+function Start () {
+
+	anim = GetComponent(Animator);
+	grounded = false;
+
+}
+
+function FixedUpdate () {
+	
+	if(grounded == false)
+	{
+		transform.Translate(Vector3.up * -10 * Time.deltaTime);
+	}
+	
+	transform.Translate(Vector3.right * 10 * Time.deltaTime * Input.GetAxis("Horizontal"));
+	
+	
+	if(grounded == true && Input.GetKeyDown(KeyCode.UpArrow))
+	{
+		transform.Translate(Vector3.up * 250 * Time.deltaTime);
+	}
+
+	anim.SetBool("WalkingLeft",false);
+	anim.SetBool("WalkingRight",false);
+	
+	if(Input.GetAxis("Horizontal")<0)
+	{
+		anim.SetBool("WalkingLeft",true);
+	}
+	
+	if(Input.GetAxis("Horizontal")>0)
+	{
+		anim.SetBool("WalkingRight",true);
+	}
+	grounded = false;
+}
+
+
+function OnTriggerEnter(other:Collider)
+{
+	if (other.gameObject.tag == "platform")
+	{
+		if (transform.position.y > other.transform.position.y)
+		{
+			grounded = true;
+		}
+	}
+}
+
+function OnTriggerStay(other:Collider)
+{
+	if (other.gameObject.tag == "platform")
+	{
+		if(transform.position.y > other.transform.position.y)
+		{
+			grounded = true;
+		}
+	}
+
+}
+
+
+
+
+
